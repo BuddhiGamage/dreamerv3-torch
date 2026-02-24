9396c035971f5f38fc9d4c1853d25202d3faa2c5
BuddhiGamage <buddhigamage217@gmail.com>
2026-02-24 23:28:27 +1100

    fixed FutureWarning: `torch.cuda.amp.autocast(args...)` is deprecated. Please use `torch.amp.autocast('cuda', args...)` instead issue

--- train_wm_offline_success_rgb_all5_tasks.py
+++ train_wm_offline_success_rgb_all5_tasks.py
@@ -559,10 +559,10 @@ def parse_hw(s: str) -> Tuple[int, int]:
 
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument("--data_root", type=str, default="/data/home/buddhig/data_all")
-    ap.add_argument("--out_root", type=str, default="/data/home/buddhig/projects/dreamer_fiper_offline/all5_tasks")
-    # ap.add_argument("--tasks", type=str, nargs="*", default=["pretzel", "push_chair", "push_t", "sorting", "stacking"])
-    ap.add_argument("--tasks", type=str, nargs="*", default=[ "stacking"])
+    ap.add_argument("--data_root", type=str, default="/home/s447658/project/fiper/data")
+    ap.add_argument("--out_root", type=str, default="/home/s447658/projects/dreamer_fiper_offline/all5_tasks")
+    ap.add_argument("--tasks", type=str, nargs="*", default=["pretzel", "push_chair", "push_t", "sorting", "stacking"])
+    # ap.add_argument("--tasks", type=str, nargs="*", default=[ "stacking"])
 
     ap.add_argument("--batch_size", type=int, default=8)
     ap.add_argument("--seq_len", type=int, default=16)
